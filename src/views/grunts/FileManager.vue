03ebfdd3970fabe9fb6941c4e48ae2ccd9701ea4
我爱秋葵 <49117752+woaiqiukui@users.noreply.github.com>
2024-03-14 16:32:14 +0800

    新增文件管理测试模块

--- src/views/grunts/FileManager.vue
+++ src/views/grunts/FileManager.vue
@@ -1,49 +1,82 @@
 <template>
   <div>
-    <a-button @click="fetchFiles">刷新文件列表</a-button>
-    <a-list
-      bordered
-      :dataSource="files"
-      item-layout="horizontal"
+    <dx-file-manager
+      :file-system-provider="remoteProvider"
+      :on-selected-file-opened="displayImagePopup"
+      current-path="Widescreen"
     >
-      <a-list-item
-        v-for="item in files"
-        :key="item.id"
-      >
-        <a-list-item-meta
-          :title="item.name"
-          :description="`文件ID: ${item.id}`"
-        ></a-list-item-meta>
-      </a-list-item>
-    </a-list>
+      <dx-permissions
+        :create="true"
+        :copy="true"
+        :move="true"
+        :delete="true"
+        :rename="true"
+        :upload="true"
+        :download="true"
+      ></dx-permissions>
+    </dx-file-manager>
+
+    <dx-popup
+      :hide-on-outside-click="true"
+      :show-close-button="true"
+      v-model="popupVisible"
+      :title="imageItemToDisplay.name"
+      max-height="600"
+      class="photo-popup-content"
+    >
+      <template #content>
+        <img :src="imageItemToDisplay.url" class="photo-popup-image">
+      </template>
+    </dx-popup>
   </div>
 </template>
 
 <script>
+import { DxFileManager, DxPermissions } from 'devextreme-vue/file-manager'
+import { DxPopup } from 'devextreme-vue/popup'
+import 'devextreme/dist/css/dx.light.css'
+
 export default {
   name: 'FileManager',
+  components: {
+    DxFileManager,
+    DxPermissions,
+    DxPopup
+  },
   data () {
     return {
-      files: []
+      popupVisible: false,
+      imageItemToDisplay: {},
+      remoteProvider: this.initRemoteProvider()
+      // 使用 customFileSystemProvider 作为 remoteProvider 的值
+      // remoteProvider: this.customFileSystemProvider
     }
   },
   methods: {
-    fetchFiles () {
-      // 模拟异步获取文件列表的操作
-      const mockFiles = [
-        { name: '文件1.txt', id: 1 },
-        { name: '图片2.png', id: 2 },
-        { name: '文档3.doc', id: 3 }
-        // 添加更多文件对象作为示例
-      ]
-
-      setTimeout(() => {
-        this.files = mockFiles
-      }, 500) // 延迟500毫秒模拟异步加载
+    initRemoteProvider () {
+      const RemoteFileSystemProvider = require('devextreme/file_management/remote_provider').default
+      return new RemoteFileSystemProvider({
+        endpointUrl: 'https://js.devexpress.com/Demos/Mvc/api/file-manager-file-system-images'
+      })
+    },
+    displayImagePopup (e) {
+      this.imageItemToDisplay = {
+        name: e.file.name,
+        url: e.file.dataItem.url
+      }
+      this.popupVisible = true
     }
-  },
-  mounted () {
-    this.fetchFiles() // 组件加载时获取文件列表
   }
 }
 </script>
+
+<style>
+.photo-popup-content {
+  text-align: center;
+}
+
+.photo-popup-content .photo-popup-image {
+  height: 100%;
+  max-width: 100%;
+}
+</style>
